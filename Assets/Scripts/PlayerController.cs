7b3d39e4d05453b9c5c02aa9683cfd029e863daa
Sean Miller <seanmiller999@gmail.com>
2022-06-07 13:24:53 -0700

    Implement pixel perfect canvas

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,23 @@ public class PlayerController : MonoBehaviour
 {
     [Header ("Sprite")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private GameObject actionHotspot;
 
-    private Animator Animator;
-    private Rigidbody2D Rigidbody2D;
+    private Animator animator;
+    private new Rigidbody2D rigidbody2D;
+
+    private float pixelsPerUnit;
+    private Vector2 direction, position;
+    private Vector3 hotspotOffset;
 
     private void Awake() {
-        Animator = GetComponent<Animator>();
-        Rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start() {
+        //hotspotOffset = actionHotspot.transform.localPosition;
+        pixelsPerUnit = GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
     }
 
     private void Update() {
@@ -19,16 +29,25 @@ public class PlayerController : MonoBehaviour
 
         bool moveX = horizInput != 0; // Signals moving on X axis, X higher priority than Y
         bool moving = moveX || vertInput != 0;
-        var direction = new Vector2(moveX ? horizInput : 0, moveX ? 0 : vertInput);
+        direction = new Vector2(moveX ? horizInput : 0, moveX ? 0 : vertInput);
 
         // Maintain values when idle so the player continues to face the direction they were moving
         if (moving) {
-            Animator.SetFloat("Horizontal", direction.x);
-            Animator.SetFloat("Vertical", direction.y);
+            animator.SetFloat("Horizontal", direction.x);
+            animator.SetFloat("Vertical", direction.y);
+            //actionHotspot.transform.transform.localPosition = hotspotOffset + (Vector3)direction;
         }
-        Animator.SetBool("Moving", moving);
+        animator.SetBool("Moving", moving);
+    }
 
-        Rigidbody2D.velocity = moveSpeed * direction;
+    private void FixedUpdate() {
+        // keep track of the player's "real" underlying position,
+        position += moveSpeed * Time.fixedDeltaTime * direction;
+        // but only move the actual player object by an integer # of pixels
+        float i_x = Mathf.FloorToInt(position.x * pixelsPerUnit);
+        float i_y = Mathf.FloorToInt(position.y * pixelsPerUnit);
+        var p = new Vector2(i_x, i_y) / pixelsPerUnit;
+        rigidbody2D.MovePosition(p);
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
