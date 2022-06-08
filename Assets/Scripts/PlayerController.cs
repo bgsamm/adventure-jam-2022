6e16bdeffb12e05d427cedf6357561ada740b485
gjanderso <42012696+gjanderso@users.noreply.github.com>
2022-06-08 19:12:34 -0400

    :bug: Fixed player movement with collisions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,16 +10,11 @@ public class PlayerController : MonoBehaviour
     private new Rigidbody2D rigidbody2D;
 
     private float pixelsPerUnit;
-    private Vector2 direction, position;
-    private Vector3 hotspotOffset;
+    private Vector2 direction;
 
     private void Awake() {
         animator = GetComponent<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
-    }
-
-    private void Start() {
-        //hotspotOffset = actionHotspot.transform.localPosition;
         pixelsPerUnit = GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
     }
 
@@ -27,27 +22,10 @@ public class PlayerController : MonoBehaviour
         float horizInput = Input.GetAxisRaw("Horizontal"),
             vertInput = Input.GetAxisRaw("Vertical");
 
-        bool moveX = horizInput != 0; // Signals moving on X axis, X higher priority than Y
-        bool moving = moveX || vertInput != 0;
-        direction = new Vector2(moveX ? horizInput : 0, moveX ? 0 : vertInput);
-
-        // Maintain values when idle so the player continues to face the direction they were moving
-        if (moving) {
-            animator.SetFloat("Horizontal", direction.x);
-            animator.SetFloat("Vertical", direction.y);
-            //actionHotspot.transform.transform.localPosition = hotspotOffset + (Vector3)direction;
-        }
-        animator.SetBool("Moving", moving);
-    }
-
-    private void FixedUpdate() {
-        // keep track of the player's "real" underlying position,
-        position += moveSpeed * Time.fixedDeltaTime * direction;
-        // but only move the actual player object by an integer # of pixels
-        float i_x = Mathf.FloorToInt(position.x * pixelsPerUnit);
-        float i_y = Mathf.FloorToInt(position.y * pixelsPerUnit);
-        var p = new Vector2(i_x, i_y) / pixelsPerUnit;
-        rigidbody2D.MovePosition(p);
+        animator.SetFloat("Horizontal", horizInput * moveSpeed);
+        animator.SetFloat("Vertical", vertInput * moveSpeed);
+        animator.SetBool("Moving", horizInput != 0 || vertInput != 0);
+        rigidbody2D.velocity = new Vector2(horizInput * moveSpeed, vertInput * moveSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
