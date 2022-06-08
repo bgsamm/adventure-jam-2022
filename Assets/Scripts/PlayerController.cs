d8f28efc0a3e586f29cce415e092cc382691d41c
gjanderso <42012696+gjanderso@users.noreply.github.com>
2022-06-08 19:41:20 -0400

    :sparkles: Collider offsets :bug: Changed back to uni-directional movement (bi-directional shakes)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,35 +1,43 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [Header ("Sprite")]
     [SerializeField] private float moveSpeed;
-    [SerializeField] private GameObject actionHotspot;
+
+    [Header ("Collider")]
+    [SerializeField] private float xOffsetRaw;
+    [SerializeField] private float yOffsetRaw;
 
     private Animator animator;
     private new Rigidbody2D rigidbody2D;
-
-    private float pixelsPerUnit;
-    private Vector2 direction;
+    private new CapsuleCollider2D collider;
 
     private void Awake() {
         animator = GetComponent<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
-        pixelsPerUnit = GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
+        collider = GetComponent<CapsuleCollider2D>();
     }
 
     private void Update() {
         float horizInput = Input.GetAxisRaw("Horizontal"),
             vertInput = Input.GetAxisRaw("Vertical");
-        bool moving = horizInput != 0 || vertInput != 0;
+        bool movingX = horizInput != 0, movingY = vertInput != 0;
 
-        if (moving)
-        {
-            animator.SetFloat("Horizontal", horizInput * moveSpeed);
-            animator.SetFloat("Vertical", vertInput * moveSpeed);
+        if (movingX || movingY)
+        {   // Sets blend tree floats and collider offsets, prioritizes horizontal
+            animator.SetFloat("Horizontal", horizInput);
+            animator.SetFloat("Vertical", vertInput);
+
+            float xOffset = movingX ? Math.Sign(horizInput) * xOffsetRaw : horizInput;
+            float yOffset = movingX ? 0 : Math.Sign(vertInput) * yOffsetRaw;
+            collider.offset = new Vector2(xOffset, yOffset);
         }
-        animator.SetBool("Moving", moving);
-        rigidbody2D.velocity = new Vector2(horizInput * moveSpeed, vertInput * moveSpeed);
+        animator.SetBool("Moving", movingX || movingY);
+
+        // prioritizes moving horizontally over vertically. Moving in both shakes screen
+        rigidbody2D.velocity = new Vector2(movingX ? horizInput * moveSpeed : 0, movingX ? 0 : vertInput * moveSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
