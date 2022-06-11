a3e85021452d360966fb64df06f1e6fd745a3692
Sean Miller <seanmiller999@gmail.com>
2022-06-11 07:45:56 -0700

    Create seed items & set placeholder inventory sprites

---
+++ Assets/Scripts/Garden/Seed.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Seed", menuName = "Seed")]
+public class Seed : Item
+{
+    public Plant plant;
+}
