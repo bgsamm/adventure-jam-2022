018dad004a67aa04bc82288099c22a547eb215ce
Sean Miller <seanmiller999@gmail.com>
2022-06-12 23:38:08 -0700

    Set up tree stages in garden scene w/ dynamic z-sorting

---
+++ Assets/Scripts/Misc/DynamicZSort.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DynamicZSort : MonoBehaviour
+{
+    private GameObject player;
+    private SpriteRenderer spriteRenderer;
+    private int playerSortOrder;
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        // this assumes the player's sort order never changes
+        playerSortOrder = player.GetComponent<SpriteRenderer>().sortingOrder;
+    }
+
+    private void Update() {
+        if (player.transform.position.y > spriteRenderer.transform.position.y)
+            spriteRenderer.sortingOrder = playerSortOrder + 1;
+        else
+            spriteRenderer.sortingOrder = playerSortOrder - 1;
+    }
+}
