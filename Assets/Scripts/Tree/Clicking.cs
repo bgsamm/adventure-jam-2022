e505c4b130848846a9a24b235de4731c77ba252f
gjanderso <42012696+gjanderso@users.noreply.github.com>
2022-06-15 21:18:25 -0400

    :sparkles: Basic logic for clicking bird/letter

---
+++ Assets/Scripts/Tree/Clicking.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicking : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<TreeScene>().ClickedObject(gameObject.name);
+    }
+}
